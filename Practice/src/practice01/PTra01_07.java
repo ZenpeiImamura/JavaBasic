bfeae3a6bd3893fea1a8cb5d942c17fda7514840
DESKTOP-CPIIR7V\zenpeiimamura <massive2018@gmail.com>
2017-11-17 12:25:10 +0900

    Part02-06まで終了

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,16 +21,24 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
+		bo=true;
+		by=78;
+		s=287;
+		c='9';
+		i=999999999;
+		l=9999999999L;
+		f=2.3F;
+		d=1.0;
 
 		// ★ それぞれの変数の中身を出力してください
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -10,13 +10,14 @@ public class PTra02_01 {
 	public static void main(String[] args) {
 
 		// ★ 20を5で割った値を出力してください
-
+		int i=20; int j=5;
+		System.out.println(i/j);
 
 		// ★ 126に13を掛けた値を出力してください
-
-
+		int a=126; int b=13;
+		System.out.println(a*b);
 		// ★ 97を6で割ったときの余りを出力してください
-
-
+		int c=97; int d=6;
+		System.out.println(c%d);
 	}
 }
